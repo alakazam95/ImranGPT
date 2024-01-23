a19af3729a57b8e7f773a1c21a5d42ba3e5aeebd
Jab00mba <93002793+Jab00mba@users.noreply.github.com>
2024-01-23 16:58:34 +0300

    Update subscription.py

--- subscription.py
+++ subscription.py
@@ -1,17 +1,27 @@
 import datetime
 from datetime import datetime, timedelta
+
+from aiogram import types
+
 import data.creator as db
+from config import bot, dp
 
 db_creator = db.dbCreator()
 
 
-def activate_subscription(user_id, type='free'):
+def check_subscription_status(user_id):
+    return db_creator.get_subscription_type(user_id)
+
+
+def activate_subscription(user_id):
+    sub_type = db_creator.get_subscription_type(user_id)
+    print(sub_type)
     """Активирует бесплатную подписку для пользователя."""
-    if type == 'paid':
+    if sub_type == 'paid':
         new_limit_update_date = datetime.now() + timedelta(days=30)  # Дата обновления через месяц
         db_creator.set_user_limit(user_id, -1)
     else:
         new_limit_update_date = datetime.now() + timedelta(days=7)  # Дата обновления через неделю
         db_creator.set_user_limit(user_id, 50000)
-    db_creator.set_subscription_type(user_id, type)
+    db_creator.set_subscription_type(user_id, sub_type)
     db_creator.set_limit_update_date(user_id, (new_limit_update_date.strftime('%Y-%m-%d %H:%M:%S')))
