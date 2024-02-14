bf7f2d16178666c5612b7a446b3c82684b99195c
Jab00mba <93002793+Jab00mba@users.noreply.github.com>
2024-02-14 03:23:00 +0300

    Update ask.py

--- handlers/ask.py
+++ handlers/ask.py
@@ -1,12 +1,1 @@
-from aiogram import types
-from config import dp
 
-@dp.message_handler(commands=['ask'], chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
-async def ask_command_in_group(message: types.Message):
-    pass
-    # Логика обработки команды в групповом чате
-
-@dp.message_handler(commands=['ask'])
-async def ask_command_elsewhere(message: types.Message):
-    # Сообщение, отправляемое, если команда вызвана вне группового чата
-    await message.reply("Эта команда доступна только в групповых чатах.")
